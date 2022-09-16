a24b5df98005f91cf163e50adde0f951cb9eadb0
itsJRillo <jmorillolabour@gmail.com>
2022-09-16 18:23:39 +0200

    Initial commit

---
+++ app/src/main/java/AMS2/UF1/MainActivity.java
@@ -0,0 +1,33 @@
+package AMS2.UF1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.tostadora);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast();
+            }
+        });
+    }
+
+    public void Toast(){
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
